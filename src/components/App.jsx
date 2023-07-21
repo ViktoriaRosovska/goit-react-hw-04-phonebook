2c885511a50a91b6b6dc03f0242cfac2833a4336
ViktoriaRosovska <121065008+ViktoriaRosovska@users.noreply.github.com>
2023-07-21 19:13:27 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,133 @@
+import { Component } from 'react';
+
+import * as services from 'services/notify';
+import { nanoid } from 'nanoid';
+
+import {
+  AddContactWrapper,
+  ContactsWrapper,
+  Container,
+  HeaderApp,
+  HeaderContacts,
+} from './App.styled';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactsList/ContactList';
+
+const CONTACT_KEY = 'contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+    order: 'az',
+  };
+
+  onStorageContact = (contactName, contactNumber) => {
+    if (this.state.contacts.find(user => user.name === contactName)) {
+      return services.Notify.warning(`${contactName} is already in contacts`);
+    }
+    if (this.state.contacts.find(user => user.number === contactNumber)) {
+      return services.Notify.warning(
+        `This number: ${contactNumber} is already in contacts`
+      );
+    }
+    const user = {
+      name: contactName,
+      number: contactNumber,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, user],
+      filter: '',
+    }));
+  };
+
+  componentDidMount() {
+    const getContacts = JSON.parse(window.localStorage.getItem(CONTACT_KEY));
+    if (getContacts) {
+      this.setState({
+        contacts: getContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      window.localStorage.setItem(
+        CONTACT_KEY,
+        JSON.stringify(this.state.contacts)
+      );
+    }
+  }
+
+  onFilterContact = e => {
+    const filter = e.target.value.toLowerCase();
+    this.setState({ filter });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter, order } = this.state;
+    const filtered = contacts.filter(el =>
+      el.name.toLowerCase().includes(filter)
+    );
+    if (order === 'az') {
+      return filtered.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (order === 'za') {
+      return filtered.sort((a, b) => b.name.localeCompare(a.name));
+    }
+
+    return filtered;
+  };
+
+  onSortContacts = order => {
+    this.setState({ order });
+  };
+
+  onDeleteContact = user => {
+    services.Confirm.show(
+      `Delete contact`,
+      `Are you sure you want to delete contact ${user.name}?`,
+      'Yes',
+      'No',
+      () => {
+        services.Notify.info(`Contact ${user.name} was deleted`);
+        return this.setState(prevState => ({
+          contacts: prevState.contacts.filter(
+            contact => contact.id !== user.id
+          ),
+        }));
+      }
+    );
+  };
+
+  render() {
+    return (
+      <Container>
+        <AddContactWrapper>
+          <HeaderApp>Phonebook</HeaderApp>
+          <ContactForm onStorageContact={this.onStorageContact} />
+        </AddContactWrapper>
+        <ContactsWrapper>
+          <Filter
+            onFilterContact={this.onFilterContact}
+            filter={this.state.filter}
+          />
+          <HeaderContacts>Contacts</HeaderContacts>
+          <ContactList
+            contactList={this.getFilteredContacts()}
+            onSortContacts={this.onSortContacts}
+            onDeleteContact={this.onDeleteContact}
+            hasContacts={this.state.contacts.length > 0}
+          />
+        </ContactsWrapper>
+      </Container>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,54 @@
+import PropTypes from 'prop-types';
+import { FormWrapper, FormInput, FormButton } from './ContactForm.styled';
+import { Component } from 'react';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    const userName = e.currentTarget.elements.name.value;
+    const userNumber = e.currentTarget.elements.number.value;
+
+    this.setState({
+      name: userName,
+      number: userNumber,
+    });
+    this.props.onStorageContact(userName, userNumber);
+    e.currentTarget.reset();
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <FormWrapper>
+          <label htmlFor="formName">Name</label>
+          <FormInput
+            id="formName"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <label htmlFor="phone">Number</label>
+          <FormInput
+            type="tel"
+            name="number"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            required
+          />
+          <FormButton type="submit">Add contact</FormButton>
+        </FormWrapper>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onStorageContact: PropTypes.func.isRequired,
+};
